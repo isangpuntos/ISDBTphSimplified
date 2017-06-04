8c17f7664f5cb91f941c215cf852172dc39b143a
isangpuntos <isang.puntos@gmail.com>
2017-06-04 19:55:53 +0800

    First Commit
    First Commit

---
+++ ISDBTphSimplified/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ISDBTphSimplified
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axVLCPlugin21 = new AxAXVLC.AxVLCPlugin2();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axVLCPlugin21
+            // 
+            this.axVLCPlugin21.Enabled = true;
+            this.axVLCPlugin21.Location = new System.Drawing.Point(0, 0);
+            this.axVLCPlugin21.Name = "axVLCPlugin21";
+            this.axVLCPlugin21.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin21.OcxState")));
+            this.axVLCPlugin21.Size = new System.Drawing.Size(284, 261);
+            this.axVLCPlugin21.TabIndex = 0;
+            this.axVLCPlugin21.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.axVLCPlugin21_PreviewKeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(22, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 39);
+            this.label1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axVLCPlugin21);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin21;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ISDBTphSimplified/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISDBTphSimplified
+{
+    public partial class Form1 : Form
+    {
+        List<string[]> channelList = new List<string[]>();
+        int currentChannel = 0;
+        string aspectRatio = "16:9";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //TopMost = true;
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            axVLCPlugin21.Size = Screen.PrimaryScreen.Bounds.Size;
+            try
+            {
+                string line = "";
+                System.IO.StreamReader channelFile = new System.IO.StreamReader("ChannelsList.txt");
+                System.IO.StreamReader currentConfig = new System.IO.StreamReader("CurrentConfig.txt");
+
+
+                while ((line = channelFile.ReadLine()) != null)
+                {
+                    channelList.Add(line.Split('`'));
+                }
+
+                channelFile.Close();
+
+                while ((line = currentConfig.ReadLine()) != null)
+                {
+                    currentChannel = Convert.ToInt32(line.Split('`')[0]);
+                    aspectRatio = line.Split('`')[1];
+                    axVLCPlugin21.video.aspectRatio = aspectRatio;
+                    if (!Regex.Match(line, "[0-9]+[`][0-9]+[:][0-9]+").Success)
+                    {
+                        aspectRatio = "16:9";
+                        axVLCPlugin21.video.aspectRatio = "16:9";
+                        for (int i = 0; i < channelList.Count; i++)
+                        {
+                            if (channelList[i][2].Contains("TV5"))
+                            {
+                                currentChannel = i;
+                            }
+
+                        }
+                    }
+                }
+
+                currentConfig.Close();
+
+                File.WriteAllText(@"CurrentConfig.txt", currentChannel.ToString() + "`" + aspectRatio);
+                string[] vOptions = { @":dvb-frequency=" + channelList[currentChannel][0], @":dvb-bandwidth=6", @":program=" + channelList[currentChannel][1] };
+                axVLCPlugin21.playlist.stop();
+                axVLCPlugin21.playlist.items.clear();
+                axVLCPlugin21.playlist.add(@"dvb-t://", null, vOptions);
+                axVLCPlugin21.playlist.play();
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+        private void axVLCPlugin21_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            int keyVal = e.KeyValue;
+
+            if (keyVal == 27)
+            {
+                this.Close();
+            }
+            else if (keyVal > 47 && keyVal < 58)
+            {
+                if (!timer1.Enabled)
+                {
+                    timer1.Enabled = true;
+                    timer1.Start();
+                }
+                label1.Text += (e.KeyValue - 48).ToString();
+            }
+            else if (keyVal == 13)
+            {
+                if(label1.Text != "" && Convert.ToInt32(label1.Text) != currentChannel && Convert.ToInt32(label1.Text) < channelList.Count)
+                {
+                    currentChannel = Convert.ToInt32(label1.Text);
+                    string[] vOptions = { @":dvb-frequency="+channelList[currentChannel][0], @":dvb-bandwidth=6", @":program=" + channelList[currentChannel][1]};
+                    axVLCPlugin21.playlist.stop();
+                    axVLCPlugin21.playlist.items.clear();
+                    axVLCPlugin21.playlist.add(@"dvb-t://", null, vOptions);
+                    axVLCPlugin21.playlist.play();
+                    timer1.Enabled = false;
+                    timer1.Stop();
+                }
+                label1.Text = "";
+            }
+            else if(keyVal == 38 || keyVal == 40)
+            {
+                currentChannel = (keyVal == 40? (currentChannel + 1) % channelList.Count : currentChannel + (currentChannel == 0? channelList.Count - 1 : -1)) ;
+                string[] vOptions = { @":dvb-frequency=" + channelList[currentChannel][0], @":dvb-bandwidth=6", @":program=" + channelList[currentChannel][1] };
+                axVLCPlugin21.playlist.stop();
+                axVLCPlugin21.playlist.items.clear();
+                axVLCPlugin21.playlist.add(@"dvb-t://", null, vOptions);
+                axVLCPlugin21.playlist.play();
+                timer1.Enabled = false;
+                timer1.Stop();
+                bool isSuccessWriting = false;
+                do
+                {
+                    try
+                    {
+                        File.WriteAllText(@"CurrentConfig.txt", currentChannel.ToString() + "`" + aspectRatio);
+                        isSuccessWriting = true;
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                } while (!isSuccessWriting);
+            }
+            else if(keyVal == 82)
+            {
+                if(aspectRatio == "4:3")
+                {
+                    aspectRatio = "16:9";
+                }
+                else if (aspectRatio == "16:9")
+                {
+                    aspectRatio = "16:10";
+                }
+                else if (aspectRatio == "16:10")
+                {
+                    aspectRatio = "4:3";
+                }
+                axVLCPlugin21.video.aspectRatio = aspectRatio;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bool isSuccessWriting = false;
+            if (label1.Text != "" && Convert.ToInt32(label1.Text) != currentChannel && Convert.ToInt32(label1.Text) < channelList.Count)
+            {
+                currentChannel = Convert.ToInt32(label1.Text);
+                string[] vOptions = { @":dvb-frequency=" + channelList[currentChannel][0], @":dvb-bandwidth=6", @":program=" + channelList[currentChannel][1] };
+                axVLCPlugin21.playlist.stop();
+                axVLCPlugin21.playlist.items.clear();
+                axVLCPlugin21.playlist.add(@"dvb-t://frequency=" + channelList[currentChannel][0], null, vOptions);
+                axVLCPlugin21.playlist.play();
+                label1.Text = "";
+                do
+                {
+                    try
+                    {
+                        File.WriteAllText(@"CurrentConfig.txt", currentChannel.ToString() + "`" + aspectRatio);
+                        isSuccessWriting = true;
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                } while (!isSuccessWriting);
+                timer1.Enabled = false;
+                timer1.Stop();
+            }
+        }
+    }
+}
